3349cf7886573302f6b252d33c7c287ff318a0e3
Ubuntu <userazure@AKSHITA-VMM.swmh05wgw1fe3dbxvgbszh2xeg.gx.internal.cloudapp.net>
2024-07-21 13:03:48 +0000

    Update app.py to display my name

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Akshita Jain! How are you?'
-
+    return 'Hello, Akshita Jain!'
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
